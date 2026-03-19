04d91e0100d6214b2212dae2ddfeefa73e42af41
Guido <mguilhermetavares@gmail.com>
2026-03-19 17:37:31 +0000

    fix: add next-sanity metadata and viewport exports to studio page

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,7 +1,8 @@
-"use client";
-
 import { NextStudio } from "next-sanity/studio";
+import { metadata, viewport } from "next-sanity/studio";
 import config from "../../../../sanity.config";
+
+export { metadata, viewport };
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
